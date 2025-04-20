5565a1bcfe9b8382f876c707080ac44ca4d150e0
Aditya Damerla <75409196+zesty-genius128@users.noreply.github.com>
2025-04-19 20:51:35 -0400

    flake8 refactors

--- models/biomedbert.py
+++ models/biomedbert.py
@@ -6,13 +6,13 @@ device = torch.device("cpu")  # force CPU execution
 
 class BiomedBERT:
     def __init__(self):
-        self.model_name= "microsoft/BiomedNLP-BiomedBERT-base-uncased-abstract"
+        self.model_name = "microsoft/BiomedNLP-BiomedBERT-base-uncased-abstract"
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
         self.model = AutoModel.from_pretrained(self.model_name).to(device)
 
     def get_embedding(self, text):
         inputs = self.tokenizer(
-            text,return_tensors="pt",padding=True,truncation=True,max_length=512
+            text, return_tensors="pt", padding=True, truncation=True, max_length=512
         )
         with torch.no_grad():
             outputs = self.model(**inputs)
